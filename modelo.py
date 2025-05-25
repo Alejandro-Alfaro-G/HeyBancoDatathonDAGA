f6def1c8138d11342ca4f59a86cf9753c872f17f
Alejandro-Alfaro_G <alejandroalfaro7905@gmail.com>
2025-05-24 22:32:25 -0600

    recuento e instalación de librerias para el analisis de preliminar de los datos

--- modelo.py
+++ modelo.py
@@ -5,3 +5,12 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error, mean_squared_error
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBRegressor
+
+print("¡Todas las librerías están instaladas!")
+
+# usen el siguiente comando para instalar las dependencias necesarias:
+# pip install pandas numpy matplotlib seaborn scikit-learn xgboost lightgbm imbalanced-learn jupyterlab tqdm
